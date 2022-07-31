e8f6ce5c631a2e8977dfe80b37188507f70b7c88
TheMrTaik <76868386+TheMrTaik@users.noreply.github.com>
2022-07-31 18:04:33 +0200

    Possibility to power ON and OFF via MQTT (Test)
    Added the first possibility to send ON and OFF command to the DSP-W215 via MQTT

--- app.js
+++ app.js
@@ -8,12 +8,59 @@
 
 var soapclient = require('./js/soapclient');
 var fs = require('fs');
+const mqtt = require('mqtt')
 
 var OUTPUT_FILE = "result.txt";
 var LOGIN_USER = "admin";
-var LOGIN_PWD = "<PIN CODE>";
-var HNAP_URL = "http://192.168.1.128/HNAP1";
+var LOGIN_PWD = "<PIN_CODE>";
+var HNAP_URL = "http://<IP_OF_DSP-W215>/HNAP1";
 var POLLING_INTERVAL = 60000;
+
+//Parametri MQTT
+const host = '<IP_OF_MQTT_BROKER>'
+const port = '1883'
+const clientID = 'DSP-W215'
+const subTopic = 'cmnd/DSP-W215'
+
+//Connessione MQTT
+const connectURL = `mqtt://${host}:${port}`
+
+const client = mqtt.connect(connectURL, {
+        clientID,
+        clean: true,
+        connectTimeout: 4000,
+        username: 'DSP-W215',
+        password: 'null',
+        reconnectPeriod: 1000,
+})
+
+//Sub Topic
+client.on('connect', () => {
+        console.log('Connected')
+        client.subscribe([subTopic], () => {
+                console.log(`Subscribe to topic '${subTopic}'`)
+        })
+})
+
+//Ricezione Messaggi MQTT
+var messaggio = "null";
+
+client.on('message', (topic, payload) => {
+//      console.log('Received Message:', topic, payload.toString())
+        messaggio = payload.toString();
+        console.log(`Var impostata a '${messaggio}'`)
+
+        //Accensione
+        if(messaggio == "ON") {
+                soapclient.on();
+        }
+
+        //Spegnimento
+        if(messaggio == "OFF") {
+                soapclient.off();
+        }
+})
+
 var args = process.argv.slice(2);
 
 soapclient.login(LOGIN_USER, LOGIN_PWD, HNAP_URL).done(function (status) {
