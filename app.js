91ab926721155e3f354d633a5e216b23d03842b1
TheMrTaik <76868386+TheMrTaik@users.noreply.github.com>
2022-07-31 15:53:37 +0200

    Added args functionality
    Added functionality to send command and receive updates via "args", this is the second step to adapt code for MQTT publish / subscription. Thanks rhapen from https://thediyiot.com/2018/10/d-link-dsp-215

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 /**
- * Tool for reading data from D-Link DSP-W215 Home Smart Plug.
+ * Tool for send command and receive updates from D-Link DSP-W215 Home Smart Plug via MQTT.
  *
  * Usage: enter your PIN code to LOGIN_PWD, change value of HNAP_URL according to your device settings.
  *
  * @type {exports|module.exports}
  */
+
 var soapclient = require('./js/soapclient');
 var fs = require('fs');
 
@@ -13,38 +14,50 @@ var LOGIN_USER = "admin";
 var LOGIN_PWD = "<PIN CODE>";
 var HNAP_URL = "http://192.168.1.128/HNAP1";
 var POLLING_INTERVAL = 60000;
+var args = process.argv.slice(2);
 
 soapclient.login(LOGIN_USER, LOGIN_PWD, HNAP_URL).done(function (status) {
-    if (!status) {
-        throw "Login failed!";
-    }
-    if (status != "success") {
-        throw "Login failed!";
-    }
-    start();
+        if (!status) {
+                throw "Login failed!";
+        }
+
+        if (status != "success") {
+                throw "Login failed!";
+        }
+
+        //Accensione
+        if(args == "ON") {
+                soapclient.on();
+        }
+
+        //Spegnimento
+        if(args == "OFF") {
+                soapclient.off();
+        }
+
+        //Stato Presa
+        if(args == "STATE") {
+                soapclient.state().done(function(state) {
+                        if(state == "true") {
+                                console.log("ON");
+                        } else {
+                                console.log("OFF");
+                        }
+                });
+        }
+
+        //Consumo Attuale
+        if(args == "POWER") {
+                soapclient.consumption().done(function(power) { console.log(power); });
+        }
+
+        //Consumo Totale
+        if(args == "TOTAL_POWER") {
+                soapclient.totalConsumption().done(function(power) { console.log(power); });
+        }
+
+        //Temperatura Presa
+        if(args == "TEMP") {
+                soapclient.temperature().done(function(temperature) { console.log(temperature); });
+        }
 });
-
-function start(){
-    soapclient.on().done(function (result){
-        console.log(result);
-        read();
-    })
-};
-
-function read() {
-    soapclient.consumption().done(function (power) {
-        soapclient.temperature().done(function (temperature) {
-            console.log(new Date().toLocaleString(), power, temperature);
-            save(power, temperature);
-            setTimeout(function () {
-                read();
-            }, POLLING_INTERVAL);
-        });
-    })
-}
-
-function save(power, temperature) {
-    fs.writeFile(OUTPUT_FILE, new Date().toLocaleString() + ";" + power + ";" + temperature + "\r\n", {flag: "a"}, function (err) {
-        if (err) throw err;
-    })
-}
